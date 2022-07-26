2c481f937d5aaf39edabc3db64c3d3319c0d98cd
Arshad561 <sarshadbasha@gmail.com>
2022-07-26 17:09:08 +0530

    adding addition function

--- src/js/helloworld.js
+++ src/js/helloworld.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+function add(num1, num2) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    const sum = num1 + num2;
+    console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+  } else {
+    console.log('Please provide valid numbers');
+  }
+}
+
+add(2, 3);
