fc9ae1e82ece9f2acdf16bef22d05136ea0a5579
Arshad561 <sarshadbasha@gmail.com>
2022-08-03 14:16:48 +0530

    added sum

--- src/js/helloworld.js
+++ src/js/helloworld.js
@@ -9,4 +9,13 @@ function add(num1, num2) {
   }
 }
 
+function sum(num1, num2) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    const sum = num1 + num2;
+    console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+  } else {
+    console.log('Please provide valid numbers');
+  }
+}
+
 add(2, 3);
